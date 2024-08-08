fa690821adb3545dc60600c82794f6adaadc1a5f
Gabriel Furrer <gfurrer@fyo.com>
2024-08-07 22:35:01 -0300

    Ejercicio 1 y 2

---
+++ Practica1/Controllers/Ej1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet("numeros")]
+        
+        public int Get(int num1, int num2,int num3)
+        {
+           return num1 + num2 + num3;
+        }
+
+
+    }
+}
---
+++ Practica1/Controllers/Ej2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej2Controller : ControllerBase
+    {
+
+        [HttpGet("NombreCiudad")]
+        public string Get (string nombre, string ciudad)
+        {
+            return $"Hola {nombre}, Bienvenido a {ciudad}";
+        }
+
+
+    }
+}
