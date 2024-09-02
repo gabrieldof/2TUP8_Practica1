2880c34fe3128bf71067e30465c3fda3e44c4a21
Gabriel Furrer <gfurrer@fyo.com>
2024-09-02 18:23:25 -0300

    Ejercicios LINQ 6 y 7

---
+++ Practica1/Controllers/LINQEj6Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Practica1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LINQEj6Controller : ControllerBase
+    {
+        // GET: api/<LINQEj6Controller>
+        [HttpGet]
+        public ActionResult Get([FromQuery] List<string> palabras)
+        {
+            var nuevasPalabras = palabras.Select(pal => pal.Replace("ia", "*")).ToList();
+            return Ok(nuevasPalabras);
+        }
+
+
+    }
+}
---
+++ Practica1/Controllers/LINQEj7Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Practica1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LINQEj7Controller : ControllerBase
+    {
+        // GET: api/<LINQEj7Controller>
+        [HttpGet]
+        public ActionResult Get([FromQuery] string frase)
+        {
+            var palabras = frase.Split(' ');
+            var palabrasMayus = palabras.Where(pal => string.Equals(pal, pal.ToUpper(), StringComparison.Ordinal));
+
+            return Ok(palabrasMayus);
+        }
+
+
+    }
+}
